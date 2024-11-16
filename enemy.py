4232e4a5f4855bf168111b0949bb6fef8d68e72a
KirilDev <alexprogrammer2023@gmail.com>
2024-11-16 13:02:53 +0200

    prototype we have, but code won't work when we uncomment everything

--- enemy.py
+++ enemy.py
@@ -1,39 +1,39 @@
-#Should handle mainly the standart stuff, visual stuff later. 
-from map import *
-with open("Resources/Misc/sight_map.json") as file:
-    sight_data=json.loads(file.read())
-class Entity:
-    def __init__(self,type):
-        self.type=type
-        self.active=False
-        self.x=0
-        self.y=0
-        self.z=0
-        self.squares_seen=[]
-        self.walls_seen=[]
-        self.health_level=100
-    def update_vision(self,heightmap):
-        visible_tiles=set()
-        checked_tiles=set()
-        blocked_tiles=set()
-        walls_seen=set()
-        map_size=(len(heightmap[0]),len(heightmap))
-        for i in sight_data["Order Of Checking"]:
-            stri=str(i)
-            if not stri in checked_tiles: #let's keep it as a heightmap, because it will get very convoluted very quickly 
-                if not map_size[1]>self.z+i[2]>-1: #Several Break Chains to test out of bounds squares
-                    break
-                if not map_size[0]>self.x+i[0]>-1:
-                    break
-                #print(i,heightmap[self.z+i[2]][self.x+i[0]],self.x+i[0],self.z+i[2])
-                if heightmap[self.z+i[2]][self.x+i[0]]>=self.y+i[1]+1: #The terrain is blocked
-                    for ii in sight_data["Sight Map"][stri]:
-                        blocked_tiles.add(str(ii))
-                        checked_tiles.add(str(ii))
-                    walls_seen.add(stri)
-                elif heightmap[self.z+i[2]][self.x+i[0]]<=self.y+i[1]: #The Terrain isn't blocked
-                    visible_tiles.add(stri)
-                checked_tiles.add(stri)
-        self.squares_seen=[[self.x,self.y,self.z]]+[[int(ii)+[self.x,self.y,self.z][II] for II,ii in enumerate(i[1:-1].split(","))] for i in visible_tiles]
-        self.walls_seen=[[self.x,self.y,self.z]]+[[int(ii)+[self.x,self.y,self.z][II] for II,ii in enumerate(i[1:-1].split(","))] for i in walls_seen]
-        self.squares_un_seen=[[int(ii)+[self.x,self.y,self.z][II] for II,ii in enumerate(i[1:-1].split(","))] for i in blocked_tiles]
+# #Should handle mainly the standart stuff, visual stuff later. 
+# from map import *
+# with open("Resources/Misc/sight_map.json") as file:
+#     sight_data=json.loads(file.read())
+# class Entity:
+#     def __init__(self,type):
+#         self.type=type
+#         self.active=False
+#         self.x=0
+#         self.y=0
+#         self.z=0
+#         self.squares_seen=[]
+#         self.walls_seen=[]
+#         self.health_level=100
+#     def update_vision(self,heightmap):
+#         visible_tiles=set()
+#         checked_tiles=set()
+#         blocked_tiles=set()
+#         walls_seen=set()
+#         map_size=(len(heightmap[0]),len(heightmap))
+#         for i in sight_data["Order Of Checking"]:
+#             stri=str(i)
+#             if not stri in checked_tiles: #let's keep it as a heightmap, because it will get very convoluted very quickly 
+#                 if not map_size[1]>self.z+i[2]>-1: #Several Break Chains to test out of bounds squares
+#                     break
+#                 if not map_size[0]>self.x+i[0]>-1:
+#                     break
+#                 #print(i,heightmap[self.z+i[2]][self.x+i[0]],self.x+i[0],self.z+i[2])
+#                 if heightmap[self.z+i[2]][self.x+i[0]]>=self.y+i[1]+1: #The terrain is blocked
+#                     for ii in sight_data["Sight Map"][stri]:
+#                         blocked_tiles.add(str(ii))
+#                         checked_tiles.add(str(ii))
+#                     walls_seen.add(stri)
+#                 elif heightmap[self.z+i[2]][self.x+i[0]]<=self.y+i[1]: #The Terrain isn't blocked
+#                     visible_tiles.add(stri)
+#                 checked_tiles.add(stri)
+#         self.squares_seen=[[self.x,self.y,self.z]]+[[int(ii)+[self.x,self.y,self.z][II] for II,ii in enumerate(i[1:-1].split(","))] for i in visible_tiles]
+#         self.walls_seen=[[self.x,self.y,self.z]]+[[int(ii)+[self.x,self.y,self.z][II] for II,ii in enumerate(i[1:-1].split(","))] for i in walls_seen]
+#         self.squares_un_seen=[[int(ii)+[self.x,self.y,self.z][II] for II,ii in enumerate(i[1:-1].split(","))] for i in blocked_tiles]
--- player.py
+++ player.py
@@ -1,12 +1,18 @@
 from entity import *
 from enemy import *
 import random
+import pygame
+
+# pygame.init()
+# screen = pygame.display.set_mode((2000, 1000))
 #unseen_surface=pygame.Surface((128,128))
 #unseen_surface.set_alpha()
 class Player:
     def __init__(self):
         self.entity=Entity("Pis Dirst")
         self.entity.active=True
+        self.entity.health_level=100
+        self.player_rect = pygame.Rect(0,0,0,0)
         #self.discovered_surface(())
     def move(self,heightmap,direction):
         if direction=="Down":
@@ -26,33 +32,41 @@ class Player:
                 if heightmap.heightmap[self.entity.z][self.entity.x-1]<=self.entity.y:
                     self.entity.x-=1
         self.entity.update_vision(heightmap.heightmap)
-class EnemyController:
-    def __init__(self, x, y, speed):
-        self.x = x
-        self.y = y
-        self.speed = speed
-        self.direction = 'right'
 
-    def enemyMove(self):
-        if self.direction == 'right':
-            self.x += self.speed
-        elif self.direction == 'left':
-            self.x -= self.speed
-        elif self.direction == 'up':
-            self.y -= self.speed
-        elif self.direction == 'down':
-            self.y += self.speed
+# class EnemyController:
+#     def __init__(self, x, z, speed, player_health):
+#         self.x = x
+#         self.z = z
+#         self.speed = speed
+#         self.direction = 'right'
+#         self.player_health = 0
 
-        if random.randint(0, 100) < 10:
-            self.direction = random.choice(['up', 'down', 'left', 'right'])
+#     def enemyMove(self):
+#         if self.direction == 'right':
+#             self.x += self.speed
+#         elif self.direction == 'left':
+#             self.x -= self.speed
+#         elif self.direction == 'up':
+#             self.z -= self.speed
+#         elif self.direction == 'down':
+#             self.z += self.speed
+
+#         if random.randint(0, 100) < 10:
+#             self.direction = random.choice(['up', 'down', 'left', 'right'])
     
-    def draw(self, screen):
-        pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, 20, 20))
+#     def draw(self, screen):
+#         pygame.draw.rect(screen, (255, 0, 0), (self.x, self.z, 20, 20))
 
-enemy = EnemyController(100, 100, 2)
+# enemy = EnemyController(100, 100, 1, 100)
 
-while True:
-    enemy.enemyMove()
-    enemy.draw(screen)
-    if player_rect.colliderect(pygame.Rect(enemy.x, enemy.y, 20, 20)):
-        player_health -= enemy.damage
+# while True:
+#     enemy.enemyMove()
+#     # enemy.draw(screen)
+
+#     # Entity.x += Entity.entity.x
+#     # Entity.z += Entity.entity.z
+#     # # Entity.player_rect.x = Entity.entity.x
+#     # # Entity.player_rect.z = Entity.entity.z
+    
+#     # # if player_rect.colliderect(pygame.Rect(enemy.x, enemy.y, 20, 20)):
+#     # #     player_health -= enemy.damage
